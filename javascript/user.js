ed7b1b51c066a6ca2bbe90dfacc249543694505a
Antoine Merino Aguilar <antoine.merino@hotmail.com>
2024-12-12 15:16:21 +0100

    Final Version - deploy page
    modification pour permettre de deploy

--- javascript/user.js
+++ javascript/user.js
@@ -3,5 +3,5 @@ let nameKid=document.querySelector("#nom");
 
 buttonRegisterKid.addEventListener("click",e=>{
     const encodedData = encodeURIComponent(nameKid.value==""?"kid":nameKid.value);
-    window.location.href = `index.html?isKidCreated=true&kidName=${encodedData}`;
+    window.location.href = `home.html?isKidCreated=true&kidName=${encodedData}`;
 });
